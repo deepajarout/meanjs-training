d335efc5f5e6f1b24925ace6c676fff645487040
Manohar Negi <manohar@hurreh.com>
2018-03-22 09:41:23 +0530

    added day nodejs stream code

--- 27-buffers/create.js
+++
@@ -1,9 +0,0 @@
-// create an uninitiated Buffer of 10 octets
-var buf = new Buffer(10);
-
-// create a Buffer from a given array
-var buf = new Buffer([10, 20, 30, 40, 50]);
-
-// create a Buffer from a given string and optionally encoding type
-var buf = new Buffer("Simply Easy Learning", "utf-8");
-
